e9cf2d0c48f1ddd6ad8383ad9cec4766065cfd23
RudyKr <rudy.kruissel@gmail.com>
2017-09-25 12:45:18 +0200

    test ls 21

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -6,17 +6,114 @@ import java.util.ArrayList;
 
 public class DefaultTeam
 {
+
 public ArrayList<Point> calculDominatingSet (ArrayList<Point> points, int edgeThreshold)
 {
-	ArrayList<Point> result = (ArrayList<Point>) points.clone();
-	return glouton(result, points, edgeThreshold);
+	return localSearch(points, edgeThreshold);
 }
 
-private ArrayList<Point> glouton (ArrayList<Point> result, ArrayList<Point> points, int edgeThreshold)
+private ArrayList<Point> localSearch (ArrayList<Point> graphe, int edgeThreshold)
 {
-	ArrayList<Point> ed = new ArrayList<Point>();
+	int              i  = 0;
+	ArrayList<Point> ds = (ArrayList<Point>) graphe.clone();
+	while (Evaluation.isValid(graphe, ds, edgeThreshold))
+	{
+		System.out.println("localsearch n°" + ++i + "   taille de dominantSet : " + ds.size());
+		ds = localsearch2_1(ds, graphe, edgeThreshold);
+	}
+	return ds;
+}
 
-	return result;
+private ArrayList<Point> localsearch2_1 (ArrayList<Point> dominantSet, ArrayList<Point> graphe, int edgeThreshold)
+{
+	ArrayList<Point> rest   = (ArrayList<Point>) graphe.clone();
+	int              tooFar = 3 * edgeThreshold;
+	for (Point p1 : dominantSet)
+	{
+		for (Point p2 : dominantSet)
+		{
+			if (p1.equals(p2))
+				continue;
+			if (p1.distance(p2) > tooFar)
+				continue;
+
+			for (Point x : rest)
+			{
+				ArrayList<Point> dsClone = (ArrayList<Point>) dominantSet.clone();
+				dsClone.remove(p1);
+				dsClone.remove(p2);
+				dsClone.add(x);
+				if (Evaluation.isValid(graphe, dsClone, edgeThreshold))
+					return dsClone;
+			}
+		}
+	}
+	return dominantSet;
+}
+
+private ArrayList<Point> localsearch3_2 (ArrayList<Point> dominantSet, ArrayList<Point> graphe, int edgeThreshold)
+{
+	ArrayList<Point> rest   = (ArrayList<Point>) graphe.clone();
+	int              tooFar = 3 * edgeThreshold;
+	for (Point p1 : dominantSet)
+	{
+		for (Point p2 : dominantSet)
+		{
+			if (p1.equals(p2))
+				continue;
+			for (Point p3 : dominantSet)
+			{
+				if (p1.equals(p2) || p2.equals(p3))
+					continue;
+				if (p1.distance(p2) > tooFar || p2.distance(p3) > tooFar || p2.distance(p3) > tooFar)
+					continue;
+
+				for (Point x : rest)
+				{
+					for (Point y : rest)
+					{
+						if (x.equals(y))
+							continue;
+
+						ArrayList<Point> dsClone = (ArrayList<Point>) dominantSet.clone();
+						dsClone.remove(p1);
+						dsClone.remove(p2);
+						dsClone.remove(p3);
+						dsClone.add(x);
+						dsClone.add(y);
+						if (Evaluation.isValid(graphe, dsClone, edgeThreshold))
+							return dsClone;
+					}
+				}
+			}
+		}
+	}
+	return dominantSet;
+}
+
+private ArrayList<Point> glouton (ArrayList<Point> points, int edgeThreshold)
+{
+	ArrayList<Point> reste     = (ArrayList<Point>) points.clone();
+	ArrayList<Point> dominants = new ArrayList<Point>();
+	while (!Evaluation.isValid(points, dominants, edgeThreshold))
+	{
+
+		dominants.add(reste.remove(0));
+		System.out.println(dominants.size());
+	}
+	return dominants;
+}
+
+private ArrayList<Point> gloutonbis (ArrayList<Point> points, int edgeThreshold)
+{
+	ArrayList<Point> reste     = (ArrayList<Point>) points.clone();
+	ArrayList<Point> dominants = new ArrayList<Point>();
+	while (Evaluation.isValid(points, reste, edgeThreshold))
+	{
+		dominants.add(reste.remove(0));
+		System.out.println(dominants.size());
+	}
+	return reste;
 }
 
 //FILE PRINTER
